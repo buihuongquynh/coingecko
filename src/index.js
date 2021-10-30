106f03910bb816a8884e9b91a77d48ec18f89bba
buihuongquynh <buigiangquynh@gmail.com>
2021-10-30 07:30:53 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -34,12 +34,7 @@ const analytics = getAnalytics(app);
 ReactDOM.render(
   <React.StrictMode>
     <Store><App /></Store>
-    
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
