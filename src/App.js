f2c5f1ff4876164c063f458147ccead78b41e111
buihuongquynh <buigiangquynh@gmail.com>
2021-10-30 11:45:41 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from "react";
 import Header from "./components/header/header";
 import Login from "./Page/Login";
-import Home from "./Page/Home"
-import './App.css'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import Home from "./Page/Home";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Router basename="/">
+      <Router basename="/coingecko">
         <Header />
         <div className="content">
           <Switch>
-          <Route path="/login" exact component={Login} />
-          <Route path="/home" component={Home}/>
+            <Route exact path="/" exact component={Login} />
+            <Route path="/home" component={Home} />
           </Switch>
         </div>
-      </Router >
+      </Router>
     </div>
   );
 }
